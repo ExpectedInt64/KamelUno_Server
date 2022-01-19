9985c158e1ad3f25ee5795ad20e348d32b9c724e
Volkan Isik <s180103@student.dtu.dk>
2022-01-19 08:04:40 +0100

    Oprydning i backend code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,67 +4,63 @@ import org.jspace.FormalField;
 import org.jspace.SequentialSpace;
 import org.jspace.SpaceRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        final String PORT = "9001";
-        System.out.println("Server starting at " + PORT);
+        final int PORT = 9001;
+        //Server Space Repository and the space between server and client
         SpaceRepository kamelUnoServerRepository = new SpaceRepository();
         SequentialSpace requestSpace = new SequentialSpace();
         SequentialSpace serverToLobbyManager = new SequentialSpace();
         kamelUnoServerRepository.add("requestSpace",requestSpace);
-        kamelUnoServerRepository.addGate("tcp://server:" + PORT + "/?keep");
-        List<String> players = new ArrayList<>();
-        List<String> lobbies = new ArrayList<>();
 
-        new Thread(new LobbyManager(serverToLobbyManager,kamelUnoServerRepository,players)).start();
+        System.out.println("Server starting at port: " + PORT);
+        kamelUnoServerRepository.addGate("tcp://server:" + PORT + "/?keep");
+
+        //Start LobbyManager in another thread.
+        new Thread(new LobbyManager(serverToLobbyManager,kamelUnoServerRepository)).start();
 
         while (true){
             try {
+                //Listen to request's from Clients. Template: (String,String,String)
                 Object[] request = requestSpace.get(new FormalField(String.class), new FormalField(String.class), new FormalField(String.class));
                 String requestType = (String) request[0];
                 String requestVerb = (String) request[1];
                 String requestArgument = (String) request[2];
                 System.out.println("Serving request " + requestType + " : " + requestVerb + " -> " + requestArgument);
 
-                //TODO: Sender vi requesten kun til lobbymanager så skal denne switch case fjernes.
+                //For now server serves only requests to LobbyManager
+                if ("lobby".equals(requestType)) {
 
-                switch (requestType){
-                    case "lobby":
-                        if(requestVerb.equals("getLobbies")){
-                            System.out.println("Sending getlobbies to lobbymanager");
-                            serverToLobbyManager.put(requestVerb, requestArgument);
-                            Object[] response = serverToLobbyManager.get(new ActualField("getLobbies"),new FormalField(String[].class));
-                            requestSpace.put(response[0],response[1]);
-                            System.out.println("done");
-                        }else {
-                            serverToLobbyManager.put(requestVerb, requestArgument);
-                            Object[] response = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new FormalField(String.class));
-                            String the_if = (String) response[2];
-                            if (the_if.equals("if")) {
-                                Object[] response2client = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new ActualField("oklobby"));
-                                requestSpace.put(response2client[2]);
-                            }else{
-                                Object[] response2client = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new ActualField("koybbol"));
-                                requestSpace.put(response2client[2]);
-                            }
+                    //This requestVerb returns all lobbies that were created. Output: String array.
+                    if (requestVerb.equals("getLobbies")) {
+                        System.out.println("Sending getlobbies to lobbymanager");
+                        serverToLobbyManager.put(requestVerb, requestArgument);
+                        Object[] response = serverToLobbyManager.get(new ActualField("getLobbies"), new FormalField(String[].class));
+                        requestSpace.put(response[0], response[1]);
+                        System.out.println("done");
+                    } else {
+                        //Other requests such as create and join lobby is processed here and the relevant response sendt to Client.
+                        serverToLobbyManager.put(requestVerb, requestArgument);
+                        Object[] response = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new FormalField(String.class));
+                        String the_if = (String) response[2];
+                        //Success response with if
+                        if (the_if.equals("if")) {
+                            Object[] response2client = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new ActualField("oklobby"));
+                            requestSpace.put(response2client[2]);
+                        } else {
+                            //Error response
+                            Object[] response2client = serverToLobbyManager.get(new FormalField(String.class), new FormalField(String.class), new ActualField("koybbol"));
+                            requestSpace.put(response2client[2]);
                         }
-                        break;
-                    default:
-                        System.out.println("request type not found");
+                    }
+                } else {
+                    System.out.println("request type not found");
+                    requestSpace.put("koybbol");
                 }
 
             }catch (Exception e){
                 e.printStackTrace();
             }
         }
-
-        //Server.getInstance().addChatRoom();
-        //Server.getInstance().addChatRoom();
-        //System.out.println("Hello from bottom")
-
-
     }
 }
--- src/main/java/chat/LobbyManager.java
+++ src/main/java/chat/LobbyManager.java
@@ -1,79 +1,77 @@
 package chat;
-
 import org.jspace.*;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
-public class LobbyManager implements Runnable{
+public class LobbyManager implements Runnable {
     private Space server_LobbyManager;
-    private List<String> players;
     private SpaceRepository spaceRepository;
     private SequentialSpace lobbies;
 
-    public LobbyManager(Space server_LobbyManager, SpaceRepository spaceRepository, List<String> players) {
+    public LobbyManager(Space server_LobbyManager, SpaceRepository spaceRepository) {
         this.server_LobbyManager = server_LobbyManager;
-        this.players = players;
         this.spaceRepository = spaceRepository;
         lobbies = new SequentialSpace();
     }
 
     @Override
     public void run() {
+        //Lobby Id is integer starting from 0.
         int lobbyID = 0;
         String lobbyURI;
-        while (true){
-            try{
+        while (true) {
+            try {
+                //LobbyManager server requests coming from server with template (String, String)
                 Object[] request = server_LobbyManager.get(new FormalField(String.class), new FormalField(String.class));
                 String requestType = (String) request[0];
                 String requestArgument = (String) request[1];
                 System.out.println("Processing request at LobbyManager " + requestType + " -> " + requestArgument);
-                switch (requestType){
+
+                // 3 request types are served looking at first element in tuple.
+                switch (requestType) {
+
+                    //Creates a lobby
                     case "createLobby":
-                        // TODO: skal man kunne oprette en lobby med given id eller skal man bare oprette som næste ledig id.
-                        Object[] the_lobby = lobbies.queryp(new ActualField(requestArgument),new FormalField(Integer.class));
-                        if (the_lobby != null) {
-                            System.out.println("The lobby does exist. Sending error response.");
-                            server_LobbyManager.put(requestType,requestArgument,"else");
-                            server_LobbyManager.put(requestType,requestArgument,"koybbol");
-                        }else {
-                            lobbyURI = "tcp://127.0.0.1:9001/lobby" + lobbyID + "?keep";
-                            System.out.println("Setting up lobby space " + lobbyURI + "...");
-                            SequentialSpace lobby = new SequentialSpace();
-                            spaceRepository.add("lobby" + lobbyID, lobby);
-                            new Thread(new lobbyWaiter(lobby, lobbyID)).start();
-                            lobbies.put(""+lobbyID,1);
-                            lobbyID++;
-                            server_LobbyManager.put(requestType,requestArgument,"if");
-                            server_LobbyManager.put(requestType,requestArgument,"oklobby");
-                        }
+                        lobbyURI = "tcp://127.0.0.1:9001/lobby" + lobbyID + "?keep";
+                        System.out.println("Setting up lobby space " + lobbyURI + "...");
+                        SequentialSpace lobby = new SequentialSpace();
+                        spaceRepository.add("lobby" + lobbyID, lobby);
+                        //lobbywaiter is started with a new thread thus making every lobby as a private space and private waiter.
+                        new Thread(new lobbyWaiter(lobby, lobbyID,spaceRepository)).start();
+                        lobbies.put("" + lobbyID, 1);
+                        lobbyID++;
+                        server_LobbyManager.put(requestType, requestArgument, "if");
+                        server_LobbyManager.put(requestType, requestArgument, "oklobby");
                         break;
+
+                    //Checks that a Client can or cannot join a lobby
                     case "joinLobby":
-                        Object[] the_lobby2 = lobbies.queryp(new ActualField(requestArgument),new FormalField(Integer.class));
+                        Object[] the_lobby2 = lobbies.queryp(new ActualField(requestArgument), new FormalField(Integer.class));
                         if (the_lobby2 != null) {
                             System.out.println("The lobby is found sending URL");
                             int numOfPlayer = (int) the_lobby2[1];
-                            if(numOfPlayer < 4) {
+                            if (numOfPlayer < 4) {
                                 lobbyURI = "tcp://127.0.0.1:9001/lobby" + the_lobby2[0] + "?keep";
                                 server_LobbyManager.put(requestType, requestArgument, "if");
                                 server_LobbyManager.put(requestType, requestArgument, "oklobby");
-                                Object[] updateLobby = lobbies.get(new ActualField(requestArgument),new FormalField(Integer.class));
+                                Object[] updateLobby = lobbies.get(new ActualField(requestArgument), new FormalField(Integer.class));
                                 int increasesize = (int) updateLobby[1];
                                 increasesize++;
-                                lobbies.put(updateLobby[0],increasesize);
+                                lobbies.put(updateLobby[0], increasesize);
 
-                            }else{
+                            } else {
                                 System.out.println("The lobby is full. Sending error response.");
-                                server_LobbyManager.put(requestType,requestArgument,"else");
-                                server_LobbyManager.put(requestType,requestArgument,"koybbol");
+                                server_LobbyManager.put(requestType, requestArgument, "else");
+                                server_LobbyManager.put(requestType, requestArgument, "koybbol");
                             }
-                        }else{
-                            System.out.println("The lobby "+ requestArgument +" does not exist. Sending error response.");
-                            server_LobbyManager.put(requestType,requestArgument,"else");
-                            server_LobbyManager.put(requestType,requestArgument,"koybbol");
+                        } else {
+                            System.out.println("The lobby " + requestArgument + " does not exist. Sending error response.");
+                            server_LobbyManager.put(requestType, requestArgument, "else");
+                            server_LobbyManager.put(requestType, requestArgument, "koybbol");
                         }
                         break;
+
+                    //Sends lobbies that were created.
                     case "getLobbies":
                         LinkedList<Object[]> lobbies = this.lobbies.queryAll(new FormalField(String.class), new FormalField(Integer.class));
                         String[] list = new String[lobbies.size()];
@@ -83,14 +81,13 @@ public class LobbyManager implements Runnable{
                             System.out.println(value);
                             list[i] = value;
                         }
-
-                        server_LobbyManager.put("getLobbies",list);
+                        server_LobbyManager.put("getLobbies", list);
                         break;
                     default:
 
                 }
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -98,42 +95,55 @@ public class LobbyManager implements Runnable{
     }
 }
 
+/**
+ *  lobbyWaiter serves four different functionalities in a lobby:
+ *  * Adds Clients to players array.
+ *  * Returns all players in a string Array.
+ *  * Broadcasts a message to all players.
+ *  * Initializes gameHandler.
+ */
 class lobbyWaiter implements Runnable {
 
     private Space lobby;
+    private SpaceRepository spaceRepository;
     private int lobbyID;
     private ArrayList<String> players;
 
-    public lobbyWaiter(Space lobby,int lobbyID) {
+    public lobbyWaiter(Space lobby, int lobbyID, SpaceRepository spaceRepository) {
         this.lobby = lobby;
         this.lobbyID = lobbyID;
+        this.spaceRepository = spaceRepository;
         this.players = new ArrayList<String>();
     }
 
     @Override
     public void run() {
         while (true) {
-            try{
+            try {
                 Object[] t = lobby.get(new FormalField(String.class), new FormalField(String.class));
                 String msg1 = (String) t[0];
                 String msg2 = (String) t[1];
-                if(msg1.equals("joined")){
-                    System.out.println("Lobby"+lobbyID+": "+msg2 + " has " + msg1);
+                if (msg1.equals("joined")) {
+                    System.out.println("Lobby" + lobbyID + ": " + msg2 + " has " + msg1);
                     players.add(msg2);
-                    lobby.put(msg2,"has joined.");
-                }else if (msg1.equals("getPlayers")){
-                    System.out.println("Lobby"+lobbyID+": get Players requested.");
+                    lobby.put(msg2, "has joined.");
+                } else if (msg1.equals("getPlayers")) {
+                    System.out.println("Lobby" + lobbyID + ": get Players requested.");
                     String[] listofplayers = players.toArray(String[]::new);
                     lobby.put(listofplayers);
-                } else{
-                    System.out.println("Lobby"+lobbyID+": "+t[0] + ":" + t[1]);
-                    for(String player : players){
+                } else if(msg1.equals("initGame")){
+                    System.out.println("Lobby" + lobbyID + ": initGame requested. Starting gameHandler in a thread.");
+                    SequentialSpace gameSpace = new SequentialSpace();
+                    spaceRepository.add("game"+lobbyID,gameSpace);
+                    //TODO: Her skal gameHandler starte.
+                    lobby.put(msg1,"go!");
+                } else {
+                    System.out.println("Lobby" + lobbyID + ": " + t[0] + ":" + t[1]);
+                    for (String player : players) {
                         lobby.put(player, t[0], t[1]);
                     }
                 }
-
-
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
 
             }
         }
